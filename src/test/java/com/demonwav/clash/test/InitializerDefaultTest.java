6dbc6948322782df5470a77ab307f624c945df7d
DemonWav <demonwav@gmail.com>
2017-06-29 00:40:22 -0500

    Add initializer + default test

---
+++ src/test/java/com/demonwav/clash/test/InitializerDefaultTest.java
@@ -0,0 +1,62 @@
+package com.demonwav.clash.test;
+
+import com.demonwav.clash.Argument;
+import com.demonwav.clash.Clash;
+import com.demonwav.clash.Initializer;
+import java.util.Objects;
+import java.util.UUID;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InitializerDefaultTest {
+
+    private static final String result = UUID.randomUUID().toString();
+    private static final String asdf = "asdf";
+
+    @Test
+    public void initializerDefaultTest() {
+        final String[] args = new String[] {};
+        Assert.assertEquals(new Bean(asdf + result), Clash.init(Bean.class, args));
+    }
+
+    private static class Bean {
+        @Argument(shortName = "r", initializer = Init.class, defaultValue = asdf)
+        private final String res;
+
+        private Bean(String res) {
+            this.res = res;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Bean{");
+            sb.append("res='").append(res).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            final Bean bean = (Bean) o;
+            return Objects.equals(res, bean.res);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(res);
+        }
+
+        private static class Init implements Initializer<String> {
+            @Override
+            public String initialize(String value) {
+                return value + result;
+            }
+        }
+    }
+}
